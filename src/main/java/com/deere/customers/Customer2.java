5b610cd26972bc2bcda393198a2ae64215606817
VENKAT <mallapallyvenkat@gmail.com>
2022-04-06 16:06:55 -0500

    Create Customer2.java

---
+++ src/main/java/com/deere/customers/Customer2.java
@@ -0,0 +1,78 @@
+package com.deere.customers;
+
+public class Customer {
+    long id;
+    String firstName;
+    String lastName;
+    String title;
+    String gender;
+    String phone;
+    String email;
+
+    public Customer(long id, String firstName, String lastName, String title, String gender, String phone, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.title = title;
+        this.gender = gender;
+        this.phone = phone;
+        this.email = email;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
