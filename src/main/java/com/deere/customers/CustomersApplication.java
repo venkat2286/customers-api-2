6ab3eee1b6316057db64496d4a5eb25fca9c7179
vm72927 <mallapallyvenkat@johndeere.com>
2022-04-09 13:20:47 -0500

    cahnges

--- src/main/java/com/deere/customers/CustomersApplication.java
+++ src/main/java/com/deere/customers/CustomersApplication.java
@@ -3,9 +3,11 @@ package com.deere.customers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@ComponentScan(basePackages = "com.deere.customers")
+//@ComponentScan(basePackages = "com.deere.customers")
 @SpringBootApplication
+@EnableSwagger2
 public class CustomersApplication {
 
 	public static void main(String[] args) {
